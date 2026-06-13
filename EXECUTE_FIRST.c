bc4cf374f2d48244205a6b035f61ac87971b40b4
benji.7z <163656970+Benji7z@users.noreply.github.com>
2026-06-13 18:49:43 +0200

    Fix typo in warning message

--- EXECUTE_FIRST.c
+++ EXECUTE_FIRST.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-  printf("\033[31;1;4mBe carefule with what files you edit since there is no:\n- Undo\n- Quit without saving\nWhich means it is easy to mess up your files\033[0m");
-  printf("You can delete this file now.");
+  printf("\033[31;1;4mBe careful with what files you edit since there is no:\n- Undo\n- Quit without saving\nWhich means it is easy to mess up your files\033[0m");
+  printf("\nYou can delete this file now.");
   return 0;
 }
