46e17f66ecbdddcd905405c4e93283f55c36d54f
benji.7z <163656970+Benji7z@users.noreply.github.com>
2026-06-13 18:44:06 +0200

    Add warning message in EXECUTE_FIRST.c

---
+++ EXECUTE_FIRST.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+  printf("\033[31;1;4mBe carefule with what files you edit since there is no:\n- Undo\n- Quit without saving\nWhich means it is easy to mess up your files\033[0m");
+  printf("You can delete this file now.");
+  return 0;
+}
