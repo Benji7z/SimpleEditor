88715c134f69eb8dd2b704cc2b80dee50ee829b7
benji.7z <163656970+Benji7z@users.noreply.github.com>
2026-06-13 19:48:10 +0200

    Remove EXECUTE_FIRST.c file

--- EXECUTE_FIRST.c
+++ EXECUTE_FIRST.c
@@ -1,7 +1,1 @@
-#include <stdio.h>
-int main(void)
-{
-  printf("\033[31;1;4mBe careful with what files you edit since there is no:\n- Undo\n- Quit without saving\nWhich means it is easy to mess up your files\033[0m");
-  printf("\nYou can delete this file now.");
-  return 0;
-}
+
